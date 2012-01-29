9e822153d713c8d9e2e65e84ba8f97adfb0cf373
Ben Noordhuis <info@bnoordhuis.nl>
2012-01-29 01:03:46 +0100

    rust-errno, first commit.

---
+++ errno.rs
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2012, Ben Noordhuis <info@bnoordhuis.nl>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#[license = "ISC"];
+
+#[link(name = "errno",
+       vers = "1.0",
+       author = "Ben Noordhuis <info@bnoordhuis.nl>")];
+
+import size_t = ctypes::size_t;
+import c_int = ctypes::c_int;
+import sbuf = str::sbuf;
+
+#[nolink]
+native mod __glibc {
+  fn __xpg_strerror_r(errnum: c_int, buf: *mutable u8, len: size_t) -> c_int;
+  fn __errno_location() -> *c_int;
+}
+
+fn errno() -> int {
+  unsafe { *__glibc::__errno_location() as int }
+}
+
+fn strerror(errnum: int) -> str {
+  let bufv = vec::init_elt_mut(1024u, 0u8);
+  let buf = ptr::mut_addr_of(bufv[0]);
+  let len = vec::len(bufv);
+
+  unsafe {
+    let r = __glibc::__xpg_strerror_r(errnum as c_int, buf, len as size_t);
+
+    if (r as bool) {
+      fail #fmt("__glibc::__xpg_strerror_r() failed [errno=%d]", errno());
+    }
+
+    ret str::from_cstr(buf as sbuf);
+  }
+}
+
+#[test]
+fn test() {
+  // FIXME chokes on localized messages
+  assert errno::strerror(22) == "Invalid argument";
+  assert errno::strerror(0) == "Success";
+}
+
+#[should_fail]
+#[test]
+fn test2() {
+  errno::strerror(-1);
+}
